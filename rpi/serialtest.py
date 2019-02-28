cb71a946bb05d723ecf9ff832ad82f8e22f9530e
CRITICALWizChess <mdvander@ualberta.ca>
2019-02-27 17:33:10 -0700

    updating some code

--- rpi/serialtest.py
+++ rpi/serialtest.py
@@ -5,7 +5,7 @@ import serial
                
 ser = serial.Serial(            
     port='/dev/serial0',
-    baudrate = 9600,
+    baudrate = 19200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
--- rpi/voicerecsamp.py
+++ rpi/voicerecsamp.py
@@ -10,23 +10,24 @@ ser = serial.Serial(
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
     timeout=1
+    )
 
 while 1:
-		# obtain audio from the microphone
-		r = sr.Recognizer()
-		with sr.Microphone() as source:
-			print("Listening")
-			audio = r.listen(source)
-		
-		# recognize speech using Sphinx
-		try:
-			Command = r.recognize_sphinx(audio) # save command as variable
-			print(Command) # print variable, this will not be in the final version
-	except sr.UnknownValueError:
-		print("Sphinx could not understand audio")
-	except sr.RequestError as e:
-		print("Sphinx error; {0}".format(e))
-		
-	ser.write(Command)
-	raw_input("")
-	
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening")
+        audio = r.listen(source)
+        
+    # recognize speech using Sphinx
+    try:
+        Command = r.recognize_sphinx(audio) +"\n" # save command as variable
+        print(Command) # print variable, this will not be in the final version
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+        
+    ser.write(Command)
+    raw_input("")
+    
