8d4a018412dd8f51d1afc713e5bad5a8876f29d1
CRITICALWizChess <mdvander@ualberta.ca>
2019-03-08 13:55:00 -0700

    Adding code and fixing bugs

--- rpi/chessrec.py
+++ rpi/chessrec.py
@@ -23,11 +23,12 @@ listenled = LED(4)
 #initialize variables
 player = "WHITE"
 convert = 0
+speak = 0
 
 while 1:
 
 	   
-	if readyspeak.is_pressed:
+	if (readyspeak.is_pressed or speak ):
 		listenled.on()
 	    # obtain audio from the microphone
         r = sr.Recognizer()
@@ -37,51 +38,76 @@ while 1:
         
         # recognize speech using Sphinx
         try:
-            Commandi = r.recognize_sphinx(audio) +"\n" # save command as variable
-            print(Commandi) # print variable, this will not be in the final version
+            Commandi = r.recognize_sphinx(audio) # save command as variable
+            print(Commandi = "\n") # print variable, this will not be in the final version
         except sr.UnknownValueError:
             print("Sphinx could not understand audio")
         except sr.RequestError as e:
             print("Sphinx error; {0}".format(e))
                 
-		listenled.blink() # processing
 		
 		#checks if string is usable (add a flag or summon to invoke the listen loop) 
-		parts = Commandi.split(" ",3)
-		convert = 1
-		
-	if convert
-		#convert to usable format
-		if parts[3] == "ONE"
-			number = "1"
-		elif parts[3] == "TWO"
-			number = "2"
-		elif parts[3] == "THREE"
-			number = "3"
-		elif parts[3] == "FOUR"
-			number = "4"
-		elif parts[3] == "FIVE"
-			number = "5"
-		elif parts[3] == "SIX"
-			number = "6"
-		elif parts[3] == "SEVEN"
-			number = "7"
-		else
-			for i in range(1,3):
-				listenled.on()
-				sleep(1)
+		if (len(Commandi) >= 16 and len(Commandi) <= 23):
+			parts = Commandi.split(" ",3)
+			if (len(parts[3]) >= 7):
+				convert = 1
+				speak = 0
+			else:
 				listenled.blink()
 				sleep(1)
-		Commando = player[0].lower()+parts[0][0].lower()+parts[2][0].lower()+number
-		if player == "WHITE"
+				speak = 1
+		else:
+			listenled.blink()
+			sleep(1)
+			speak = 1
+			
+	if convert:
+		#convert to usable format
+		listenled.off() # processing
+		if parts[3] == "ONE":
+			number = "1"
+		elif parts[3] == "TWO":
+			number = "2"
+		elif parts[3] == "THREE":
+			number = "3"
+		elif parts[3] == "FOUR":
+			number = "4"
+		elif parts[3] == "FIVE":
+			number = "5"
+		elif parts[3] == "SIX":
+			number = "6"
+		elif parts[3] == "SEVEN":
+			number = "7"
+		else:
+			listenled.blink()
+			sleep(1)
+			break
+		if parts[0] == "PAWN":
+			piece = "p"
+		elif parts[0] == "ROOK":
+			piece = "r"
+		elif parts[0] == "KNIGHT":
+			piece = "n"
+		elif parts[0] == "BISHOP":
+			piece = "b"
+		elif parts[0] == "QUEEN":
+			piece = "q"
+		elif parts[0] == "KING":
+			piece = "k"
+		else:
+			listenled.blink()
+			sleep(1)
+			break
+		Commando = player[0].lower()+piece+parts[2][0].lower()+number
+		if player == "WHITE":
 			player = "BLACK"
-		else
+		else:
 			player = "WHITE"
 		
 		ser.write(Commando)
 		sleep(1)
 		convert = 0
-		listenled.off()
+		
 		
 		
         #raw_input("")
--- rpi/rpitest.py
+++ rpi/rpitest.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 	
 #initialize variables
 player = "WHITE"
@@ -7,54 +7,73 @@ speak = 0
 
 
 
-while 1	   
+while 1:
 
 	if (1 or speak ):
 
-		Commandi = raw_input("Type your intput\n")	+"\n" # save command as variable
-		print(Commandi) # print variable, this will not be in the final version		
+		Commandi = raw_input("Type your input\n") # save command as variable
+		print("Input is " + Commandi) # print variable, this will not be in the final version		
 		
 		#checks if string is usable (add a flag or summon to invoke the listen loop) 
-		if (len(Commandi) >= 16 and len(Commandi) <= 23)
+		if (len(Commandi) >= 16 and len(Commandi) <= 23):
 			parts = Commandi.split(" ",3)
-			if (len(parts[3]) >= 7)
+			if (len(parts[3]) <= 5):
 				convert = 1
 				speak = 0
-			else	
+			else:
 				print("ERROR 3")
 				sleep(1)
 				speak = 1
-		else
+		else:
 			print("ERROR 1")
 			sleep(1)
 			speak = 1
 			
-	if convert
+	if convert:
 		#convert to usable format
 		#listenled.off() # processing
-		if parts[3] == "ONE"
+		if parts[3] == "ONE":
 			number = "1"
-		elif parts[3] == "TWO"
+		elif parts[3] == "TWO":
 			number = "2"
-		elif parts[3] == "THREE"
+		elif parts[3] == "THREE":
 			number = "3"
-		elif parts[3] == "FOUR"
+		elif parts[3] == "FOUR":
 			number = "4"
-		elif parts[3] == "FIVE"
+		elif parts[3] == "FIVE":
 			number = "5"
-		elif parts[3] == "SIX"
+		elif parts[3] == "SIX":
 			number = "6"
-		elif parts[3] == "SEVEN"
+		elif parts[3] == "SEVEN":
 			number = "7"
-		else
-			print("CONVERSION ERROR")
+		elif parts[3] == "EIGHT":
+			number = "8"
+		else:
+			print("N CONVERSION ERROR")
 			sleep(1)
-		Commando = player[0].lower()+parts[0][0].lower()+parts[2][0].lower()+number+"\n"
-		if player == "WHITE"
+			continue
+		if parts[0] == "PAWN":
+			piece = "p"
+		elif parts[0] == "ROOK":
+			piece = "r"
+		elif parts[0] == "KNIGHT":
+			piece = "n"
+		elif parts[0] == "BISHOP":
+			piece = "b"
+		elif parts[0] == "QUEEN":
+			piece = "q"
+		elif parts[0] == "KING":
+			piece = "k"
+		else:
+			print("P CONVERSION ERROR")
+			sleep(1)
+			continue
+		Commando = player[0].lower()+piece+parts[2][0].lower()+number
+		if player == "WHITE":
 			player = "BLACK"
-		else
+		else:
 			player = "WHITE"
 		
-		print("commando is" + Commando)
+		print("Commando is " + Commando)
 		
 		convert = 0
