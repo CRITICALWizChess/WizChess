9d865c41fa47627535477fb458a0b62d728315c3
CRITICALWizChess <mdvander@ualberta.ca>
2019-03-24 20:40:34 -0600

    updates

--- rpi/test_code/lcdfunc.py
+++ rpi/test_code/lcdfunc.py
@@ -60,14 +60,14 @@ def setuplcd():
   # Sets up the LCD, call this at the beginning of your program
   # Define GPIO to LCD mapping
 
-GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-GPIO.setup(LCD_E, GPIO.OUT)  # E
-GPIO.setup(LCD_RS, GPIO.OUT) # RS
-GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-GPIO.setup(LED_ON, GPIO.OUT) # Backlight enable
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+  GPIO.setup(LED_ON, GPIO.OUT) # Backlight enable
  
 # Initialise display
 lcd_init()
