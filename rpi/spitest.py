78d01dda25f9f2f5c73f5b9dc97b59f4505642bc
CRITICALWizChess <mdvander@ualberta.ca>
2019-03-26 16:09:29 -0600

    bugfixes

--- rpi/spitest.py
+++ rpi/spitest.py
@@ -1,12 +1,13 @@
 import chessrec
 
 try:
-        while True:
-            print("Welcome to the SPI testing program!\n")
-            choice=raw_input("Input your move: ")
-
-            if choice == 'exit':
-                print "\nAs you wish. Welcome back!"
-                break
-    except KeyboardInterrupt:
+    while True:
+        print("Welcome to the SPI testing program!\n")
+        choice=raw_input("Input your command or type exit to close: ")
+            
+        if choice == 'exit':
+            print( "\nAs you wish. Welcome back!")
+            break
+        chessrec.spisend(choice)
+except KeyboardInterrupt:
         sys.exit("\n\nExiting now.")
