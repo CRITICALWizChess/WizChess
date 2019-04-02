8cf6c11c9ac13a5a817b3921f4661d354672aeba
CRITICALWizChess <mdvander@ualberta.ca>
2019-04-02 14:03:49 -0600

    editing spi code

--- rpi/chessrec.py
+++ rpi/chessrec.py
@@ -35,10 +35,10 @@ def spisend(sending):
 	spi.open(0,0)
 	spi.max_speed_hz = 500000
 	spi.mode = 0
-	#msg = [int(sending[0:2]), int(sending[2:4])]
+	msg = [int(sending[0:2]), int(sending[2:4])]
 	for x in range(0,50):
-		#mseg = msg
-		mseg =[int(sending)]
+		mseg = msg
+		#mseg =[int(sending)]
 		spi.xfer2(mseg)
 	spi.close()
 
